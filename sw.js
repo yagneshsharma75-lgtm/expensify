14d47cc584335f228abf3abdcc7351bc147534ce
yagneshsharma75-lgtm <yagneshsharma75@gmail.com>
2026-09-10 17:07:46 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='expensify-v4';
+const CACHE='expensify-v5';
 const ASSETS=['./','./index.html','./login.html','./dashboard.html','./transactions.html','./analytics.html','./settings.html','./style.css','./script.js','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
